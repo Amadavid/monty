430a4f08017c5030c9f0cfd1b6990fe4e78b4917
Ghaniyat27 <ganiyat4real1@gmail.com>
2023-12-17 05:25:06 -0800

    Our Monty Group Project

---
+++ str_mon_functions.c
@@ -0,0 +1,87 @@
+#include "monty.h"
+
+/**
+ * mon_strcmp - A Function's for Comparing Two String's.
+ * @s1: Type Of Str To be Compared
+ * @s2: Type Of Str Compared
+ * Return: 0 If s1 and s2 are Equal's.
+ *         Another Value If they are Different
+ */
+int mon_strcmp(char *s1, char *s2)
+{
+	int i;
+
+	for (i = 0; s1[i] == s2[i] && s1[i]; i++)
+		;
+	if (s1[i] > s2[i])
+		return (1);
+	if (s1[i] < s2[i])
+		return (-1);
+	return (0);
+}
+
+/**
+ * _sch - Check's If a Char is Inside a String
+ * @s: String to Review
+ * @c: Char to Find
+ * Return: 1 If Success 0 If Not
+ */
+int _sch(char *s, char c)
+{
+	int cont = 0;
+
+	while (s[cont] != '\0')
+	{
+		if (s[cont] == c)
+		{
+			break;
+		}
+		cont++;
+	}
+	if (s[cont] == c)
+		return (1);
+	else
+		return (0);
+}
+
+/**
+ * _strtoky - A Function's that Cut a String
+ * into Token's Depending of The Delimiter
+ * @s: String Be to Cut in Part's
+ * @d: Delimiter's
+ * Return: First Partition
+ */
+char *_strtoky(char *s, char *d)
+{
+	static char *ultimo;
+	int i = 0, j = 0;
+
+	if (!s)
+		s = ultimo;
+	while (s[i] != '\0')
+	{
+		if (_sch(d, s[i]) == 0 && s[i + 1] == '\0')
+		{
+			ultimo = s + i + 1;
+			*ultimo = '\0';
+			s = s + j;
+			return (s);
+		}
+		else if (_sch(d, s[i]) == 0 && _sch(d, s[i + 1]) == 0)
+			i++;
+		else if (_sch(d, s[i]) == 0 && _sch(d, s[i + 1]) == 1)
+		{
+			ultimo = s + i + 1;
+			*ultimo = '\0';
+			ultimo++;
+			s = s + j;
+			return (s);
+		}
+		else if (_sch(d, s[i]) == 1)
+		{
+			j++;
+			i++;
+		}
+	}
+	return (NULL);
+}
